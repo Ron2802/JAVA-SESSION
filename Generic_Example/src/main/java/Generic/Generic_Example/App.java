ca89c73c5affb77e172ef93f82f35846c522693b
Ron_28 <ax.by.cz.4569@gmail.com>
2020-02-05 16:59:20 +0530

    # pojo classes Added

--- Generic_Example/src/main/java/Generic/Generic_Example/App.java
+++ Generic_Example/src/main/java/Generic/Generic_Example/App.java
@@ -8,15 +8,39 @@ import java.util.*;
  */
 public class App 
 {
-	private static List<Customer> l;
+	private static List<Customer> l = new ArrayList();
 	private static Service S ;
-	public <T> void Display(T element) {
+	private static Scanner scInt = new Scanner(System.in);
+	private static Scanner scStr = new Scanner(System.in);
+	
+	
+	/*public <T> void Display(T element) {
 		System.out.println(element.getClass().getName()+"__"+element);
-	}
+	}*/
+	
+	
     public static void main( String[] args )
     {
     	App p = new App();
-    /*  p.Display(10);
+    	int n;
+    	System.out.println("Enter How many records Inserted:");
+    	n = scInt.nextInt();
+    	do {
+    		System.out.println("Enter Your Name:");
+    		String name = scStr.nextLine();
+    		System.out.println("Enter Your Email:");
+    		String email = scStr.nextLine();
+    		System.out.println("Enter Your Gender:");
+    		String gender = scStr.nextLine();
+    		System.out.println("Enter Your City:");
+    		String city = scStr.nextLine();
+    		System.out.println("Enter Your Mobile Number:");
+    		Long number = scInt.nextLong();
+    		p.Init(name,email,gender,city,number);
+    		n--;
+    	}while(n>0);
+    	p.Display();
+    /* p.Display(10);
       p.Display("Ronak");
       p.Display(20.5);
            
@@ -24,13 +48,16 @@ public class App
       S= new Service<Number, Double>(10,20.56);
       S.Sum("Ronak","Krupa");
       S.Sum(10,20); */
-      p.Init();
+    
     }
     
-    public void Init() {
-    	l = new ArrayList();
-    	l.add(new Customer("Ronak","abc@gmail.com","Male",new AddressBook("Baroda",8141600024L)));
-    	System.out.println(l);
+    public void Init(String name,String email,String gender,String city,Long number) {
+    	l.add(new Customer(name,email,gender,new AddressBook(city,number)));
+    }
+    public void Display() {
+    	System.out.println("List Of Person");
+    	for(Customer c : l)
+    		System.out.println(c);
     }
     
 }
